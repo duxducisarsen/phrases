69cc3b5c719306b1cca9c3f2e2d0224f673f0f11
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2022-11-20 13:50:03 -0300

    Configuro publicadores

--- src/PhrasesServiceProvider.php
+++ src/PhrasesServiceProvider.php
@@ -13,12 +13,30 @@ class PhrasesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'phrases');
+        
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'phrase-migrations');
+
+        $this->publishes([
+            __DIR__.'/../routes/web.php' => base_path('routes/phrases.php')
+        ], 'phrase-routes');
+
+        
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/phrases'),
+        ], 'phrases-views');
+
         $this->loadViewsFrom(__DIR__.'/../resources/js', 'phrases');
+        // $this->publishes([
+        //     __DIR__.'/../public' => public_path('vendor/courier'),
+        // ], 'public');
 
-
+        
 
         /*
             ejemplos: 
