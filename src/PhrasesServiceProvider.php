4234d349880ab30fce8776cbed097f131480ab4c
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2022-11-20 10:11:11 -0300

    Agrego tag

--- src/PhrasesServiceProvider.php
+++ src/PhrasesServiceProvider.php
@@ -13,6 +13,9 @@ class PhrasesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'phrases');
+        $this->loadViewsFrom(__DIR__.'/../resources/js', 'phrases');
     }
 }
